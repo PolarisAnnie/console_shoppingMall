39aee263678bd6b5a66ca755566cd965fdb746ed
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-06-25 10:16:19 +0900

    create file

---
+++ consol_shopping_mall/bin/consol_shopping_mall.dart
@@ -0,0 +1,5 @@
+import 'package:consol_shopping_mall/consol_shopping_mall.dart' as consol_shopping_mall;
+
+void main(List<String> arguments) {
+  print('Hello world: ${consol_shopping_mall.calculate()}!');
+}
---
+++ consol_shopping_mall/test/consol_shopping_mall_test.dart
@@ -0,0 +1,8 @@
+import 'package:consol_shopping_mall/consol_shopping_mall.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
