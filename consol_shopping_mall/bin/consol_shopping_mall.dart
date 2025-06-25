c56247a2207d305382fb21ae2253bba7fdc194e1
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-06-25 15:04:26 +0900

    finish ShoppingMall class / showProducts / connectMenu

--- consol_shopping_mall/bin/consol_shopping_mall.dart
+++ consol_shopping_mall/bin/consol_shopping_mall.dart
@@ -1,26 +1,27 @@
 import 'dart:io'; //stdin.readLineSync() 활용 시 필요
 
-// ShoppingMall 클래스 정의 -2 시도하다가 메뉴로 먼저 진행
-/* class ShoppinMall{
-  List<Product>; // 데이터 담는 방법에 대해서 Map으로 하고 싶은데 어떻게 해야 할 지 고민 중
-  int totalprice = 0; // OK
+// ShoppingMall 클래스 정의
 
-  //
-  void showProducts(){
-      Map<String, int> productlist(name, price){
-        '셔츠' : 45000, 
-        '원피스' : 30000, 
-        '반팔티' : 35000,
-        '반바지' : 38000,
-        '양말' : 500
-        };
+class ShoppingMall {
+  List<Product> products = [
+    Product("셔츠", 45000),
+    Product("원피스", 30000),
+    Product("반팔티", 35000),
+    Product("반바지", 38000),
+    Product("양말", 500),
+  ];
+  int totalprice = 0;
 
-    for(String product in productlist[product])
+  void showProducts() {
+    for (Product product in products) {
+      print('${product.name} / ${product.price}원');
+    }
   }
-
-  void addTocart(){}
-  void showTotal(){}
-
+}
+/*
+void addTocart() {}
+void showTotal() {
+  print('장바구니에 ${totalprice}원 어치를 담으셨네요 !');
 }
 */
 
@@ -34,30 +35,32 @@ class Product {
 
 //함수 실행 부분
 void main() {
+  ShoppingMall shop = ShoppingMall();
   bool status = true;
   while (status) {
+    String dash = '-' * 80;
     print(
-      "------------------------------------------------------------------------------------\n [1]상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 총 가격 보기 / [4] 프로그램 종료 \n------------------------------------------------------------------------------------",
+      '$dash\n [1]상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 총 가격 보기 / [4] 프로그램 종료 \n$dash',
     );
     String? input = stdin.readLineSync();
 
     switch (input) {
       case '1':
         print('[1]상품 목록 보기');
-        //showProducts();
+        shop.showProducts();
         break;
 
       case '2':
         print('[2] 장바구니에 담기');
-        //addTocart();
+        //shop.addTocart();
         break;
 
       case '3':
         print('[3] 장바구니에 담긴 총 가격 보기');
-        //showTotal();
+        //shop.showTotal();
         break;
       case '4':
-        print('정말 종료하시겠습니까?');
+        print('정말 종료하시겠습니까? 종료하려면 5를 눌러주세요.');
         String? quitalert = stdin.readLineSync();
         if (quitalert == '5') {
           print('이용해 주셔서 감사합니다 ~ 안녕히 가세요 !');
